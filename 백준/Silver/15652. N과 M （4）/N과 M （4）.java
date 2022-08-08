7e6fa1e9f1abd96fd72f86397e25a4a4dfc8c422
Zenonylon <102449364+Zenonylon@users.noreply.github.com>
2022-08-08 15:24:47 +0900

    [Silver III] Title: N과 M (4), Time: 716 ms, Memory: 24436 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,42 @@
+// n과 m(4) silver3
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    
+    static int n, m;
+    static  int[] arr;
+
+    static void dfs(int num, int depth) {
+
+        if (depth == m) {
+			for (int i=0; i<arr.length; i++) {
+				System.out.print(arr[i] + " ");
+			}
+			System.out.println();
+            return;
+		}
+        
+		for (int i = num; i <= n; i++) {
+ 
+			arr[depth] = i;
+			dfs(i, depth + 1);
+ 
+		}
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+
+        dfs(1, 0);
+    }
+}
